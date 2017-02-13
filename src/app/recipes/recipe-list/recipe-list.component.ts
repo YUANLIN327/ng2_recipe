44852840bd005c9a45d0a1779508546be7df9dbb
Lin Yuan <yuanlin.dalian@gmail.com>
2017-02-13 17:06:39 -0500

    create first recipe.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from '../recipe';
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-
-  constructor() { }
+  recipes: Recipe[] = [];
+  recipe = new Recipe('Dummy', 'Dummy', 'http://www.haocai777.com/Article/UploadFiles2012c/201501/2015010709343083.jpg');
+  constructor() {}
 
   ngOnInit() {
   }
