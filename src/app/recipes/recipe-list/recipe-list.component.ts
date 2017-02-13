1c4c88569ac166d3ed282b3793de7d496e4bf9fb
Lin Yuan <yuanlin.dalian@gmail.com>
2017-02-13 17:26:11 -0500

    select recipe using EventEmitter

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {Recipe} from '../recipe';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../recipe';
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html'
@@ -7,9 +7,14 @@ import {Recipe} from '../recipe';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
   recipe = new Recipe('Dummy', 'Dummy', 'http://www.haocai777.com/Article/UploadFiles2012c/201501/2015010709343083.jpg');
-  constructor() {}
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+  constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
   }
 
 }
