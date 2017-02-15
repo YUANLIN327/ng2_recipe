97da4062d30b0e1834dbcaf8c497f451c7351dfe
Lin Yuan <yuanlin.dalian@gmail.com>
2017-02-15 16:40:28 -0500

    fix UI bug.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../share/ingredient';
 @Component({
   selector: 'rb-shopping-list',
   templateUrl: './shopping-list.component.html'
 })
 export class ShoppingListComponent implements OnInit {
-
+  items: Ingredient[];
   constructor() { }
 
   ngOnInit() {
