ee707990e805b64866cdbef589492d66c5861d63
Lin Yuan <yuanlin.dalian@gmail.com>
2017-02-15 15:47:37 -0500

    create unless directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { RecipeDetailComponent } from './recipes/recipe-list/recipe-detail.compo
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
 import { HighlightDirective } from './recipes/highlight.directive';
+import { UnlessDirective } from './recipes/unless.directive';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HighlightDirective } from './recipes/highlight.directive';
     RecipeDetailComponent,
     ShoppingListComponent,
     ShoppingListAddComponent,
-    HighlightDirective
+    HighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
