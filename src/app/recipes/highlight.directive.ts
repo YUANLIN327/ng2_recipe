6311123396822bc8f67b1338dd38d8e0e11083f7
Lin Yuan <yuanlin.dalian@gmail.com>
2017-02-15 15:03:16 -0500

    hostlistener, hostbinding

--- src/app/recipes/highlight.directive.ts
+++ src/app/recipes/highlight.directive.ts
@@ -1,11 +1,21 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[rbHighlight]'
 })
 export class HighlightDirective {
+  background = 'red';
+  @HostListener('mouseenter') changeColorToGreen(): void {
+    this.background = 'green';
+  }
+   @HostListener('mouseleave') changeColorBackToRed(): void {
+    this.background = 'red';
+  }
+  @HostBinding('style.background') get getBackgroundColor() {
+    return this.background;
+  }
   constructor(private elementRef: ElementRef, private renderer: Renderer) {
-    this.renderer.setElementStyle(this.elementRef.nativeElement, 'background', 'green');
+    // this.renderer.setElementStyle(this.elementRef.nativeElement, 'background', 'green');
   }
 
 }
